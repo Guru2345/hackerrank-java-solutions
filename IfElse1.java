cfb1766c3c63cde2a125feeb0c97b39de825d273
Vardhan <164505778+Guru2345@users.noreply.github.com>
2026-03-11 19:15:51 +0530

    Add files via upload

---
+++ IfElse1.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.security.*;
+
+public class IfElse1 {
+ public static void main(String[] args) {
+  DoNotTerminate.forbidExit();
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+String s = String.valueOf(n);  // or Integer.toString(n) or n + ""
+   //Write your code here
+}
